163f9f72328d7722159a6ee87b60205fdad01693
Valson Barreto <valdsonbarreto@yahoo.com.br>
2019-03-10 16:24:57 -0300

    fix home behaviour

--- app/src/main/java/com/fogoapagou/challenge193/MainActivity.kt
+++ app/src/main/java/com/fogoapagou/challenge193/MainActivity.kt
@@ -3,6 +3,8 @@ package com.fogoapagou.challenge193
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_main)
         btnRegister.setOnClickListener(this)
         btnLogin.setOnClickListener(this)
+        val watcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                btnLogin.isEnabled = txtCpf.text.isNotEmpty() && txtPassword.text.isNotEmpty()
+                btnLogin.alpha = if(btnLogin.isEnabled) 1f else .4f
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        }
+
+        txtCpf.addTextChangedListener(watcher)
+        txtPassword.addTextChangedListener(watcher)
+
+        watcher.afterTextChanged(null)
     }
 
 
@@ -24,7 +44,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 startActivity(intentRegister)
             }
             btnLogin.id -> {
-                val intentLogin = Intent(this, LoginActivity::class.java)
+                val intentLogin = Intent(this, ProfileActivity::class.java)
                 startActivity(intentLogin)
             }
         }
